1e6b67e1d8ded9131d549a29864e8a436a1ef347
qodo <qodo@google.com>
2026-01-29 14:21:58 +0300

    delete SOS logic added.

--- alumni_sos/admin.py
+++ alumni_sos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SOSRequest
 
-# Register your models here.
+@admin.register(SOSRequest)
+class SOSRequestAdmin(admin.ModelAdmin):
+    list_display = ('title', 'requester', 'created_at', 'deadline')
+    list_filter = ('created_at', 'deadline')
+    search_fields = ('title', 'description', 'requester__username')
