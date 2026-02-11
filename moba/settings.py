fcd609a2a0ca425d2113b49be20f215c61d47653
ronnie256-pro <ronniemuyinda8@gmail.com>
2026-02-11 23:45:28 +0300

    Refactor static and media file paths in settings.py
    Updated STATIC_URL and STATIC_ROOT to use BASE_DIR for better path management. Adjusted MEDIA_ROOT to also use BASE_DIR for consistency.

--- moba/settings.py
+++ moba/settings.py
@@ -134,11 +134,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'opt/mwiri-oldboys/staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join (BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/opt/mwiri-oldboys/media'
+MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
