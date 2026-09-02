6d9cb830fbaf680c0539aec027141eb8b4ff3df5
qodo <qodo@google.com>
2026-09-02 06:11:29 +0300

    added and am done with the products display

--- products/views.py
+++ products/views.py
@@ -1,12 +1,77 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView
+from django.db.models import Q
 from .models import Product, ProductImage, Service
 from .forms import ProductForm, ServiceForm
+from content.models import Category
 
 class ProductListView(ListView):
     model = Product
     template_name = "products/products.html"
     context_object_name = "products"
+    paginate_by = 12
+
+    def get_queryset(self):
+        queryset = Product.objects.select_related('category', 'owner').all()
+        
+        # Search query
+        q = self.request.GET.get('q', '').strip()
+        if q:
+            queryset = queryset.filter(Q(name__icontains=q) | Q(description__icontains=q))
+
+        # Category filter
+        category_id = self.request.GET.get('category')
+        if category_id and category_id.isdigit():
+            queryset = queryset.filter(category_id=category_id)
+
+        # Min Price filter
+        min_price = self.request.GET.get('min_price')
+        if min_price:
+            try:
+                queryset = queryset.filter(price__gte=float(min_price))
+            except ValueError:
+                pass
+
+        # Max Price filter
+        max_price = self.request.GET.get('max_price')
+        if max_price:
+            try:
+                queryset = queryset.filter(price__lte=float(max_price))
+            except ValueError:
+                pass
+
+        # Rating filter
+        rating = self.request.GET.get('rating')
+        if rating:
+            try:
+                queryset = queryset.filter(rating__gte=int(rating))
+            except ValueError:
+                pass
+
+        # Sorting
+        sort = self.request.GET.get('sort', 'newest')
+        if sort == 'price_asc':
+            queryset = queryset.order_by('price')
+        elif sort == 'price_desc':
+            queryset = queryset.order_by('-price')
+        elif sort == 'rating':
+            queryset = queryset.order_by('-rating')
+        else:  # newest
+            queryset = queryset.order_by('-id')
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['search_query'] = self.request.GET.get('q', '')
+        context['selected_category'] = self.request.GET.get('category', '')
+        context['min_price'] = self.request.GET.get('min_price', '')
+        context['max_price'] = self.request.GET.get('max_price', '')
+        context['selected_rating'] = self.request.GET.get('rating', '')
+        context['selected_sort'] = self.request.GET.get('sort', 'newest')
+        context['total_count'] = self.get_queryset().count()
+        return context
 
 
 class ServiceListView(ListView):
